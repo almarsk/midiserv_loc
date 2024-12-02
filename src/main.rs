e4ab45d417231829ae601afc98ff868aadd11c8c
Albert Maršík <albert.marsik@gmail.com>
2024-12-02 23:39:16 +0100

    ui draft

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod midi;
 // https://github.com/Boddlnagg/midir/blob/master/examples/test_play.rs
 
-use std::error::Error;
-
-use slint::{Brush, Color};
-
+use slint::{Model, ModelRc, SharedString, VecModel};
+use std::{error::Error, rc::Rc};
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let app = AppWindow::new()?;
-    app.on_go_to_main({
-        let ui_handle = app.as_weak();
-        move || {
-            if let Some(ui) = ui_handle.upgrade() {
-                ui.set_bgc(Brush::from(Color::from_argb_u8(255, 0, 255, 0)));
-            }
+
+    let exposed_devices = Rc::new(VecModel::from(vec![]));
+    app.set_exposed_devices(ModelRc::from(Rc::clone(&exposed_devices)));
+    let exposed_devices_clone = Rc::clone(&exposed_devices);
+
+    let ports_res = midi::get_ports();
+
+    if let Ok(ports) = ports_res {
+        let p = Rc::new(
+            ports
+                .iter()
+                .map(|(k, v)| SharedString::from(format!("{}|{}", v, k)))
+                .collect::<VecModel<_>>(),
+        );
+
+        app.global::<AppState>()
+            .set_midi_ports(ModelRc::from(Rc::clone(&p)));
+    }
+
+    app.global::<AppState>()
+        .on_expose_device(move |input1, input2, input3| {
+            let new_device = SharedString::from(format!("{}|{}|{}", input1, input2, input3));
+            exposed_devices_clone.push(new_device);
+        });
+
+    app.global::<AppState>().on_hide_device(move |string| {
+        let exposed_devices_clone2 = Rc::clone(&exposed_devices);
+        if let Some(i) = exposed_devices_clone2.iter().position(|s| s == string) {
+            exposed_devices.remove(i);
         }
     });
-    let _ = app.run();
 
+    let _ = app.run();
     Ok(())
 }
---
+++ src/midi.rs
@@ -0,0 +1,53 @@
+use std::{collections::HashMap, error::Error};
+
+use midir::MidiOutput;
+
+pub fn get_ports() -> Result<HashMap<String, String>, Box<dyn Error>> {
+    let midi_out = MidiOutput::new("midiserve")?;
+
+    let out_ports = midi_out.ports();
+
+    let mut ports = HashMap::new();
+
+    out_ports.clone().into_iter().for_each(|port| {
+        ports.insert(port.id(), midi_out.port_name(&out_ports[0]).unwrap());
+    });
+
+    Ok(ports)
+}
+
+/*
+    let mut conn_out = midi_out.connect(out_port, "midir-test")?;
+    println!("Connection open. Listen!");
+    {
+        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
+        let mut play_note = |note: u8, duration: u64| {
+            const NOTE_ON_MSG: u8 = 0x90;
+            const NOTE_OFF_MSG: u8 = 0x80;
+            const VELOCITY: u8 = 0x64;
+            // We're ignoring errors in here
+            let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
+            sleep(Duration::from_millis(duration * 150));
+            let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+        };
+
+        sleep(Duration::from_millis(4 * 150));
+
+        play_note(66, 4);
+        play_note(65, 3);
+        play_note(63, 1);
+        play_note(61, 6);
+        play_note(59, 2);
+        play_note(58, 4);
+        play_note(56, 4);
+        play_note(54, 4);
+    }
+    sleep(Duration::from_millis(150));
+    println!("\nClosing connection");
+    // This is optional, the connection would automatically be closed as soon as it goes out of scope
+    conn_out.close();
+    println!("Connection closed");
+    Ok(())
+}
+
+*/
