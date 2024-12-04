647230a152337677a91b1660132834d9ecea21d7
Albert Maršík <albert.marsik@gmail.com>
2024-12-04 09:19:49 +0100

    sends dummy midi, basic async setup; todo cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,25 @@ mod midi;
 
 use anyhow::Result;
 use slint::{Model, ModelRc, SharedString, VecModel};
-use std::cell::RefCell;
 use std::error::Error;
 use std::rc::Rc;
-use tokio::sync::mpsc;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
 
 slint::include_modules!();
+
+enum MidiCommand {
+    Dummy(u8),
+    Port(String),
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let app = AppWindow::new()?;
 
     // todo tokio refactor
-    let _rt = tokio::runtime::Runtime::new().unwrap();
-    let (_tx, mut _rx) = mpsc::channel::<String>(32);
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let (tx, mut rx) = mpsc::channel::<MidiCommand>(10);
+    let (shutdown_tx, mut shutdown_rx) = tokio::sync::watch::channel(false);
 
     let midi = if let Ok(m) = midi::Midi::new() {
         m
@@ -28,32 +34,98 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     if let Ok(ports) = midi.get_ports() {
-        let current_port = Rc::new(RefCell::new(String::new()));
-        let current_port_clone = Rc::clone(&current_port);
-        app.global::<AppState>().on_choose_midi_port(move |port| {
-            current_port_clone.replace(port.to_string());
-        });
         let p = Rc::new(
             ports
                 .iter()
-                .map(|(k, v)| SharedString::from(format!("{}|{}", v, k)))
+                .map(|(k, v)| SharedString::from(format!("{}|{}", k, v)))
                 .collect::<VecModel<_>>(),
         );
         p.insert(0, SharedString::from(""));
-
         app.global::<AppState>()
             .set_midi_ports(ModelRc::from(Rc::clone(&p)));
-
-        let current_port_clone = Rc::clone(&current_port);
-        app.global::<AppState>()
-            .on_send_dummy_cc(move |controller| {
-                println!(
-                    "cc {} port {}",
-                    controller,
-                    current_port_clone.borrow().split("|").last().unwrap_or("")
-                );
-            })
     }
+
+    rt.spawn(async move {
+        let midi = Arc::new(Mutex::new(midi));
+        let midi = Arc::clone(&midi);
+        loop {
+            tokio::select! {
+                // Handle MIDI commands
+                command_option = rx.recv() => {
+                    if let Some(command) = command_option {
+                        let mut midi = midi.lock().await;
+                        match command {
+                            MidiCommand::Dummy(cc) => {
+                                println!("{}", cc);
+                                midi.send_dummy_cc(cc)
+                            }
+                            MidiCommand::Port(port) => midi.update_port(port),
+                        }
+                    }
+                }
+
+                // Handle shutdown signal
+                _ = shutdown_rx.changed() => {
+                    if *shutdown_rx.borrow() {
+                        println!("Shutting down MIDI loop...");
+                        break;
+                    }
+                }
+            }
+            /*
+            tokio::select! {
+                    command_option = rx.try_recv() => {
+                     if let Ok(command) = command_option {
+                         let mut midi = midi.lock().await;
+                         match command {
+                             MidiCommand::Dummy(cc) => {
+                                 println!("{}", cc);
+                                 midi.send_dummy_cc(cc)
+                             }
+                             MidiCommand::Port(port) => midi.update_port(port),
+                         }
+                        }
+                    }
+                    _ = shutdown_rx.changed() => {
+                        if *shutdown_rx.borrow() {
+                            println!("Shutting down MIDI loop...");
+                            break;
+                        }
+                    }
+                }
+            */
+        }
+    });
+
+    let tx_clone = tx.clone();
+    app.global::<AppState>().on_choose_midi_port(move |port| {
+        let tx_clone_clone = tx_clone.clone();
+        let _ = slint::spawn_local(async move {
+            let _ = tx_clone_clone
+                .send(MidiCommand::Port(
+                    port.clone()
+                        .to_string()
+                        .split('|')
+                        .next()
+                        .unwrap_or("")
+                        .to_string(),
+                ))
+                .await;
+        });
+    });
+
+    let tx_clone = tx.clone();
+    app.global::<AppState>()
+        .on_send_dummy_cc(move |controller| {
+            println!("debug dummy app global");
+            let tx_clone_clone = tx_clone.clone();
+            let _ = slint::spawn_local(async move {
+                if let Ok(cc) = controller.clone().parse::<u8>() {
+                    let _ = tx_clone_clone.send(MidiCommand::Dummy(cc)).await;
+                    println!("debug dummy post send")
+                }
+            });
+        });
 
     let exposed_devices = Rc::new(VecModel::from(vec![]));
     app.set_exposed_devices(ModelRc::from(Rc::clone(&exposed_devices)));
@@ -78,5 +150,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     let _ = app.run();
+    let _ = shutdown_tx.send(true);
+
+    rt.block_on(async {
+        println!("Waiting for tasks to shut down...");
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    });
     Ok(())
 }
--- src/midi.rs
+++ src/midi.rs
@@ -1,7 +1,7 @@
 use midir::{MidiOutput, MidiOutputConnection};
 use std::{collections::HashMap, error::Error};
 
-// https://github.com/Boddlnagg/midir/blob/master/examples/test_play.rs
+// inspired by https://github.com/Boddlnagg/midir/blob/master/examples/test_play.rs
 
 const CC_MESSAGE: u8 = 0xB0;
 
@@ -21,65 +21,40 @@ impl Midi {
         }
     }
 
-    pub fn get_ports(self) -> Result<HashMap<String, String>, Box<dyn Error>> {
-        let out_ports = self.out.ports();
-
-        let mut ports = HashMap::new();
-
-        out_ports.clone().into_iter().for_each(|port| {
-            ports.insert(port.id(), self.out.port_name(&out_ports[0]).unwrap());
-        });
-
-        Ok(ports)
+    pub fn get_ports(&self) -> Result<HashMap<String, String>, Box<dyn Error>> {
+        if let Ok(m) = MidiOutput::new("midiserve") {
+            let out_ports = m.ports();
+            let mut ports = HashMap::new();
+            out_ports.clone().into_iter().for_each(|port| {
+                ports.insert(port.id(), self.out.port_name(&out_ports[0]).unwrap());
+            });
+            Ok(ports)
+        } else {
+            Err(format!("midi issue"))?
+        }
     }
 
-    pub fn _update_port(mut self, out_port: String) {
-        let m_port = self.out.find_port_by_id(out_port);
-        if let Some(mp) = m_port {
-            let conn_out = self.out.connect(&mp, "midiserv");
-            if let Ok(co) = conn_out {
-                self.conn = Some(co);
+    pub fn update_port(&mut self, out_port: String) {
+        if let Ok(m) = MidiOutput::new("midiserve") {
+            println!("port candidate {}", out_port);
+            let m_port = self.out.find_port_by_id(out_port);
+            if let Some(mp) = m_port {
+                if let Ok(conn_out) = m.connect(&mp, "midiserv") {
+                    self.conn = Some(conn_out);
+                    println!("yes port")
+                } else {
+                    println!("no port")
+                }
             }
         }
     }
 
-    pub fn _send_dummy_cc(self, channel: u8) {
-        if let Some(mut c) = self.conn {
+    pub fn send_dummy_cc(&mut self, channel: u8) {
+        if let Some(c) = self.conn.as_mut() {
+            println!("sendin {}", channel);
             let _ = c.send(&[CC_MESSAGE, channel, 0]);
+        } else {
+            println!("nah")
         }
     }
 }
-/*
-    println!("Connection open. Listen!");
-    {
-        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-        let mut play_note = |note: u8, duration: u64| {
-            const NOTE_ON_MSG: u8 = 0x90;
-            const NOTE_OFF_MSG: u8 = 0x80;
-            const VELOCITY: u8 = 0x64;
-            // We're ignoring errors in here
-            let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
-            sleep(Duration::from_millis(duration * 150));
-            let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
-        };
-
-        sleep(Duration::from_millis(4 * 150));
-
-        play_note(66, 4);
-        play_note(65, 3);
-        play_note(63, 1);
-        play_note(61, 6);
-        play_note(59, 2);
-        play_note(58, 4);
-        play_note(56, 4);
-        play_note(54, 4);
-    }
-    sleep(Duration::from_millis(150));
-    println!("\nClosing connection");
-    // This is optional, the connection would automatically be closed as soon as it goes out of scope
-    conn_out.close();
-    println!("Connection closed");
-    Ok(())
-}
-
-*/
